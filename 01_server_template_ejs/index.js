fc36264a7256f7fea88c8beefc6b1c19a80155f7
jyenes <javi.yenes@gmail.com>
2017-03-25 14:43:07 +0100

    solo el ununciado

--- 01_server_template_ejs/index.js
+++ 01_server_template_ejs/index.js
@@ -1,21 +1,29 @@
 'use strict';
 
-const ejs = require('ejs');
-const fs = require('fs');
 const http = require('http');
-const templateString = fs.readFileSync('views/index.ejs', 'utf-8');
 
-const params = {
-	title: 'Mi Titulo',
-	subtitle: 'El subtitulo',
-	paragraph: 'el parrafo'
-};
-const html = ejs.render(templateString, params);
+const server = http.createServer( (request, response) => {
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write('<!DOCTYPE "html">');
+  response.write('<html>');
+  response.write('<head>');
+  response.write('<title>workshop node</title>');
+  response.write('</head>');
+  response.write('<body>');
+  response.write('<h1>');
+  response.write('Titulo');
+  response.write('</h1>');
+  response.write('<h2>');
+  response.write('SubTitulo');
+  response.write('</h2>');
+  response.write('<p>');
+  response.write('mi parrafo!');
+  response.write('</p>');
+  response.write('</body>');
+  response.write('</html>');
+  response.end();
+});
 
-const server = http.createServer((req, res) => {
-	res.end(html);
-})
-
-server.listen(3000);
-
-console.log(html);
+server.listen(3000, (err, res) =>{
+  console.log('Servidor Arrancado');  
+});
